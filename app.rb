92c596e1c85480b68856b76da6c5f73022c9d592
MC Le Morvan <mclemorvan@gmail.com>
2018-10-18 12:07:55 +0200

    added UI

--- app.rb
+++ app.rb
@@ -21,6 +21,16 @@ require 'Json_scrapper.rb'
 
 hash_dep = ["http://www.annuaire-des-mairies.com/seine-saint-denis.html", "http://www.annuaire-des-mairies.com/hauts-de-seine.html", "http://www.annuaire-des-mairies.com/val-de-marne.html"]
 
-# Json.new.scrap(hash_dep)
-# Spreadsheet.new.write(Scrapper.new.result(hash_dep))
-# Csv.new.convert
+puts "what do you want to do?"
+puts " 1. get the emails from 92, 93 & 94 and put them in a Json file"
+puts " 2. put them in a google spreadsheet"
+a = gets.chomp
+
+if a == 1 then Json.new.scrap(hash_dep) end
+  
+if a == 2 then
+  Spreadsheet.new.write(Scrapper.new.result(hash_dep))
+  puts "Do you want to downlad a csv version of it?(y/n)"
+  b = gets.chomp
+  if b == "y" then Csv.new.convert end
+  end
