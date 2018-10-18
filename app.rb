96ff0c8a97952b54bfafd6957fb02ffdcdb0bbec
MC Le Morvan <mclemorvan@gmail.com>
2018-10-18 11:21:08 +0200

    spreadsheet scrap terminé

--- app.rb
+++ app.rb
@@ -12,6 +12,8 @@ require 'spreadsheet_scrapper.rb'
 $:.unshift File.expand_path("./../csv/", __FILE__)
 require 'csv.rb'
 
+# début du code
+
 
 def json_scrapper(hash)
   File.open("/Users/marie-charlotte/Documents/Code/The_Hacking_Project/Semaine_3/day4_Scrapping_email_sending/TeamRepo/db/emails.JSON", "w") do |f|
@@ -19,10 +21,16 @@ def json_scrapper(hash)
   end
 end
 
+def perform()
+puts "what do you want to do ?"
+puts "JSON scrap (1), google sheet scrap (2), google sheet csv download(3)"
+a = gets.chomp
+
+if a == 1 then json_scrapper(hash_dep) end
+if a == 2 then Spreadsheet.new.write(Scrapper.new.perform(hash_dep)) end
+if a == 3 then Csv.new.convert end
+end
+
 hash_dep = ["http://www.annuaire-des-mairies.com/seine-saint-denis.html", "http://www.annuaire-des-mairies.com/hauts-de-seine.html", "http://www.annuaire-des-mairies.com/val-de-marne.html"]
 
-# json_scrapper(hash_dep)
-
-Spreadsheet.new.write(Scrapper.new.perform(hash))
-
-# Csv.new.convert
+Spreadsheet.new.write(Scrapper.new.perform(hash_dep))
